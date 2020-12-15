ae8b4e6476ae783ca5344d7e57534d8c6b7f1877
isaaclepes <54421422+isaaclepes@users.noreply.github.com>
2020-12-15 14:36:14 -0700

    Update UI wording
    Replace Username with UUID, Minotar with Crafatar on user interface elements

--- crafatarphpbb/language/en/minotar.php
+++ crafatarphpbb/language/en/minotar.php
@@ -19,20 +19,20 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'ALLOW_SITESPLAT_MINECRAFTAVATARMINOTAR_AVATAR_DRIVER_MINECRAFTMINOTAR'	=> 'Enable Minecraft avatars via Minotar API',
-	'AVATAR_MINOTAR_ON_REGISTRATION'										=> 'Enable Minecraft avatars via Minotar API on registration',
+	'ALLOW_SITESPLAT_MINECRAFTAVATARMINOTAR_AVATAR_DRIVER_MINECRAFTMINOTAR'	=> 'Enable Minecraft avatars via Crafatar API',
+	'AVATAR_MINOTAR_ON_REGISTRATION'										=> 'Enable Minecraft avatars via Crafatar API on registration',
 
-	'SITESPLAT_MINECRAFTAVATARMINOTAR_MINECRAFTMINOTAR_TITLE'	            => 'Minecraft via Minotar API',
+	'SITESPLAT_MINECRAFTAVATARMINOTAR_MINECRAFTMINOTAR_TITLE'	            => 'Minecraft via Crafatar API',
 	'SITESPLAT_MINECRAFTAVATARMINOTAR_MINECRAFTMINOTAR_EXPLAIN'	            => 'Select the preferred options below',
 
-	'MINOTAR_AVATAR_NAME'	           => 'Minecraft Username',
+	'MINOTAR_AVATAR_NAME'	           => 'Minecraft UUID',
 	'MINOTAR_OPTIONS'		           => 'Type',
-	'AVATAR_MINOTAR_NAME_EXPLAIN'	   => 'Enter you Minecraft username',
+	'AVATAR_MINOTAR_NAME_EXPLAIN'	   => 'Enter you Minecraft UUID',
 	'AVATAR_MINOTAR_OPTIONS_EXPLAIN'   => 'Select the type of avatar',
 	
 	'MINOTAR_OPTION_AVATARS'	           => '2D Avatar',
 	'MINOTAR_OPTION_RENDERS/HEAD'	           => '3D Head',
 	'MINOTAR_OPTION_RENDERS/BODY'	           => '3D Body',
 
-	'MINOTAR_SITESPLAT'	               => 'Minecraft Minotar Extension by <a href="http://www.sitesplat.com"><strong>SiteSplat.com</strong> <img class="decoded" alt="http://www.sitesplat.com" src="https://minotar.net/avatar/clone1018/24.png"></a>'
+	'MINOTAR_SITESPLAT'	               => 'Crafatarphpbb Extension for Minecraft Avatars'
 ));
