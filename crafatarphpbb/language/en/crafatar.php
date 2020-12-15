2752ad41dfc1909bd122bfaeb07ce4fbd55de01c
isaaclepes <54421422+isaaclepes@users.noreply.github.com>
2020-12-15 15:08:51 -0700

    More display text fixes

--- crafatarphpbb/language/en/crafatar.php
+++ crafatarphpbb/language/en/crafatar.php
@@ -19,20 +19,20 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'ALLOW_CRAFATARAVATAR_CRAFATARPHPBB_AVATAR_DRIVER_MINECRAFTCRAFATAR'	=> 'Enable Minecraft avatars via Minotar API',
-	'AVATAR_CRAFATAR_ON_REGISTRATION'										=> 'Enable Minecraft avatars via Minotar API on registration',
+	'ALLOW_CRAFATARAVATAR_CRAFATARPHPBB_AVATAR_DRIVER_MINECRAFTCRAFATAR'	=> 'Enable Minecraft avatars via Crafatar API',
+	'AVATAR_CRAFATAR_ON_REGISTRATION'										=> 'Enable Minecraft avatars via Crafatar API on registration',
 
-	'CRAFATARAVATAR_CRAFATARPHPBB_MINECRAFTCRAFATAR_TITLE'	            => 'Minecraft via Minotar API',
+	'CRAFATARAVATAR_CRAFATARPHPBB_MINECRAFTCRAFATAR_TITLE'	            => 'Minecraft via Crafatar API',
 	'CRAFATARAVATAR_CRAFATARPHPBB_MINECRAFTCRAFATAR_EXPLAIN'	            => 'Select the preferred options below',
 
-	'CRAFATAR_AVATAR_NAME'	           => 'Minecraft Username',
+	'CRAFATAR_AVATAR_NAME'	           => 'Minecraft UUID',
 	'CRAFATAR_OPTIONS'		           => 'Type',
-	'AVATAR_CRAFATAR_NAME_EXPLAIN'	   => 'Enter you Minecraft username',
+	'AVATAR_CRAFATAR_NAME_EXPLAIN'	   => 'Enter you Minecraft UUID',
 	'AVATAR_CRAFATAR_OPTIONS_EXPLAIN'   => 'Select the type of avatar',
 	
 	'CRAFATAR_OPTION_AVATARS'	           => '2D Avatar',
 	'CRAFATAR_OPTION_RENDERS/HEAD'	           => '3D Head',
 	'CRAFATAR_OPTION_RENDERS/BODY'	           => '3D Body',
 
-	'CRAFATAR_CRAFATARAVATAR'	               => 'Minecraft Minotar Extension by <a href="http://www.sitesplat.com"><strong>SiteSplat.com</strong> <img class="decoded" alt="http://www.sitesplat.com" src="https://crafatar.net/avatar/clone1018/24.png"></a>'
+	'CRAFATAR_CRAFATARAVATAR'	               => 'Minecraft Crafatar Extension'
 ));
